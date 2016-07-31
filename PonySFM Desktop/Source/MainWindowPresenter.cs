9e379f889c3cb2d90a1ff39791781b209ebe7418
Gian Sass <gian.sass@outlook.de>
2016-07-31 12:05:13 +0200

    Add revision list item and make it checkable

--- PonySFM Desktop/Source/MainWindowPresenter.cs
+++ PonySFM Desktop/Source/MainWindowPresenter.cs
@@ -8,21 +8,63 @@ using System.Windows.Controls;
 
 namespace PonySFM_Desktop
 {
-    public class MainWindowPresenter : BasePresenter
+    public class RevisionListItem
     {
-        RevisionDatabase _db;
+        Revision _revision;
+        bool _checked;
 
-        public List<Revision> InstalledRevisions
+        public int ID
         {
             get
             {
-                return _db.Revisions;
+                return _revision.ID;
+            }
+        }
+
+        public bool Checked
+        {
+            get
+            {
+                return _checked;
+            }
+
+            set
+            {
+                _checked = value;
+            }
+        }
+
+        public RevisionListItem(Revision revision)
+        {
+            _revision = revision;
+        }
+    }
+
+    public class MainWindowPresenter : BasePresenter
+    {
+        List<RevisionListItem> _items = new List<RevisionListItem>();
+        RevisionDatabase _db;
+
+        public List<RevisionListItem> InstalledRevisions
+        {
+            get
+            {
+                return _items;
             }
         }
 
         public MainWindowPresenter(RevisionDatabase db)
         {
             _db = db;
+            PopulateListData();
+        }
+
+        private void PopulateListData()
+        {
+            foreach (var revision in _db.Revisions)
+            {
+                _items.Add(new RevisionListItem(revision));
+            }
         }
     }
 }
