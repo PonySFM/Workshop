acb7f2fa9fee85eaecb808cd712b4f4452736ff8
Gian Sass <gian.sass@outlook.de>
2016-08-03 19:30:50 +0200

    Add some moar tests and error codes

--- PonySFM Workshop Unit Test/GameinfoHandlerTest.cs
+++ PonySFM Workshop Unit Test/GameinfoHandlerTest.cs
@@ -25,17 +25,85 @@ namespace PonySFM_Desktop_Unit_Testing
             "   }\n" +
             "}";
 
+        private readonly string gameinfoDataComplete =
+            "\"GameInfo\"\n" +
+            "{\n" +
+            "   SearchPaths\n" +
+            "   {\n" +
+            GameinfoHandler.GameinfoLine +
+            "       Game    |gameinfo_path|.\n" +
+            "       Game    tf_movies\n" +
+            "       Game    tf\n" +
+            "   }\n" +
+            "}";
+
+        /// <summary>
+        /// Gameinfo data missing 'SearchPaths' key
+        /// </summary>
+        private readonly string gameinfoDataInvalid =
+            "\"GameInfo\"\n" +
+            "{\n" +
+            "}";
+
         [TestMethod]
-        public void TestExecution()
+        public void TestCorrectExecution()
         {
             var fs = new MockFileSystem();
             fs.CreateFile("C:\\gameinfo.txt", Encoding.UTF8.GetBytes(gameinfoData));
             var handler = new GameinfoHandler("C:\\gameinfo.txt", fs);
 
-            handler.Execute();
+            var error = handler.Execute();
+
+            Assert.AreEqual(GameinfoHandlerError.Success, error);
 
             var newdata = Encoding.UTF8.GetString(fs.ReadFile("C:\\gameinfo.txt"));
             Assert.IsTrue(newdata.IndexOf(GameinfoHandler.GameinfoLine) != -1);
+        }
+
+        [TestMethod]
+        public void TestFaultyExecution()
+        {
+            var fs = new MockFileSystem();
+            fs.CreateFile("C:\\gameinfo.txt", Encoding.UTF8.GetBytes(gameinfoDataInvalid));
+            var handler = new GameinfoHandler("C:\\gameinfo.txt", fs);
+
+            var error = handler.Execute();
+
+            Assert.AreEqual(GameinfoHandlerError.FileInvalid, error);
+        }
+
+        [TestMethod]
+        public void TestNOPExecution()
+        {
+            var fs = new MockFileSystem();
+            fs.CreateFile("C:\\gameinfo.txt", Encoding.UTF8.GetBytes(gameinfoDataComplete));
+            var handler = new GameinfoHandler("C:\\gameinfo.txt", fs);
+
+            var error = handler.Execute();
+
+            Assert.AreEqual(GameinfoHandlerError.AlreadyAdded, error);
+        }
+
+        [TestMethod]
+        public void TestBackup()
+        {
+            var fs = new MockFileSystem();
+            fs.CreateFile("C:\\gameinfo.txt", Encoding.UTF8.GetBytes(gameinfoData));
+            var handler = new GameinfoHandler("C:\\gameinfo.txt", fs);
+
+            handler.CreateBackup();
+
+            Assert.IsTrue(fs.FileExists("C:\\gameinfo.txt.bak"));
+            var backupdata = fs.ReadFile("C:\\gameinfo.txt.bak");
+
+            Assert.AreEqual(gameinfoData, Encoding.UTF8.GetString(backupdata));
+
+            /* Now test RestoreBackup */
+            fs.DeleteFile("C:\\gameinfo.txt");
+
+            handler.RestoreBackup();
+
+            Assert.IsTrue(fs.FileExists("C:\\gameinfo.txt"));
         }
     }
 }
