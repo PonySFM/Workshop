08d8cec9a9571cdf6ea3b53c4964e79538369eac
DD~! <devddstuff@gmail.com>
2016-08-03 15:03:58 -0400

    vedya gaems break

---
+++ PonySFM Workshop/DialogBox/DialogBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PonySFM_Workshop
+{
+    public enum DialogBoxResult
+    {
+        Yes, YesAll, Ok, Cancel
+    }
+
+    [Flags]
+    public enum EnableButtons
+    {
+        Yes, YesAll, Ok, Cancel
+    }
+
+    /// <summary>
+    /// Interaction logic for DialogBox.xaml
+    /// </summary>
+    public partial class DialogBox : MetroWindow
+    {
+        public DialogBox()
+        {
+            InitializeComponent();
+        }
+
+        public DialogBoxResult Result { get; private set; }
+    }
+}
