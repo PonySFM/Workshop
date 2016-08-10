784b94151c76caaff88b31eb88e546269833738f
DD~! <devddstuff@gmail.com>
2016-08-10 19:22:02 -0400

    Added the legal mentions page -- #23
    About to add the text.

---
+++ PonySFM Workshop/Licenses/Licenses.xaml.cs
@@ -0,0 +1,54 @@
+﻿using PonySFM_Workshop.Source;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PonySFM_Workshop.License
+{
+    /// <summary>
+    /// Interaction logic for Licenses.xaml
+    /// </summary>
+    public partial class Licenses : Page
+    {
+        SettingsPresenter _presenter;
+
+        public Licenses()
+        {
+            InitializeComponent();
+        }
+
+        public Licenses(ConfigHandler config)
+        {
+            _presenter = new SettingsPresenter(config);
+            _presenter.View = this;
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GoBack();
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _presenter.Reset();
+            GoBack();
+        }
+
+        private void GoBack()
+        {
+            MainWindow.Instance.SetPage("MainPage");
+        }
+    }
+}
